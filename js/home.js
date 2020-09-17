504594fd4512974738f8788ed574819161be310d
morena <morena@dsklabs.com>
2020-09-17 07:26:56 -0300

    avance 16/9

--- js/home.js
+++ js/home.js
@@ -1,51 +1,73 @@
-var search = document.getElementById("search");
-var publicaciones = document.getElementById("publicaciones");
-var filters = document.getElementById("filters");
-var searchWrp = document.getElementById("search-wrp");
-var searchForm = document.getElementById("search-form");
-var home= document.getElementById("home");
 var filterProfesion = document.getElementsByClassName("filter-profesion");
-var profesion = document.getElementsByClassName("profesion");
-var profesional = document.getElementsByClassName("profesional");
-var profesionales = document.getElementById("profesionales");
-var postularmeBtn = document.getElementsByClassName("postularme");
-
-search.addEventListener('click', openSearchForm);
-
-function openSearchForm(){
-    searchForm.style.position = "relative";
-    searchForm.style.transform = "translateX(0)";
-    publicaciones.style.position = "absolute";
-    publicaciones.style.transform = "translateX(-150vw)";
-    profesionales.style.position = "absolute";
-    profesionales.style.transform = "translateX(-150vw)";
-    filters.style.display = "none";
-    searchWrp.style.display = "none";
-}
-
-for (var y = 0; y < postularmeBtn.length; y++){
-    postularmeBtn[y].addEventListener('click', function(){
-        this.parentElement.parentElement.classList.add("postulado");
-        this.innerHTML = "Postulado";
-    })
-};
+var empresa = document.getElementById("empresa");
+var profesional = document.getElementById("profesional");
+var empresaLabel = document.getElementById("empresa-label");
+var profesionaLabel = document.getElementById("profesional-label");
+var form = document.getElementById("search-form");
+var profesionWrp = document.getElementById("profesion-wrp");
+var submit = document.getElementById("submit");
 
 for (var y = 0; y < filterProfesion.length; y++){
     filterProfesion[y].addEventListener('click', function(){
-        for (var c = 0; c < profesional.length; c++){
-            profesional[c].style.transform = "translateY(10rem)";
-        };
-        profesionales.style.position = "relative";
-        profesionales.style.transform = "translateX(0)";
-        setTimeout(function() {
-            for (var c = 0; c < profesional.length; c++){
-            profesional[c].style.transform = "translateY(0)";};
-        }, 150);
-        publicaciones.style.position = "absolute";
-        publicaciones.style.transform = "translateX(-150vw)";
-        var className = this.classList[1];
-        for (var c = 0; c < profesion.length; c++){
-            profesion[c].innerHTML = className;
-        };
-    })
-};
+        form.reset();
+        submit.value = "Encontrar profesional";
+        for (var y = 0; y < filterProfesion.length; y++){
+            filterProfesion[y].classList.remove("current");
+        }
+        this.classList.add("current");
+        if(this.classList.contains("canje")){
+            empresaLabel.innerHTML = "Ofrezco canje";
+            profesionaLabel.innerHTML = "Acepto canje";
+            profesionWrp.style.display = "block";
+        }else{
+            profesionWrp.style.display = "none";
+        }
+        if(this.classList.contains("modelo")){
+            empresaLabel.innerHTML = "Busco modelo";
+            profesionaLabel.innerHTML = "Soy modelo";
+        }
+        if(this.classList.contains("influencer")){
+            empresaLabel.innerHTML = "Busco influencer";
+            profesionaLabel.innerHTML = "Soy influencer";
+        }
+        if(this.classList.contains("actor")){
+            empresaLabel.innerHTML = "Busco actor/actriz";
+            profesionaLabel.innerHTML = "Soy actor/actriz";
+        }
+        if(this.classList.contains("fotografo")){
+            empresaLabel.innerHTML = "Busco fotógrafo/a";
+            profesionaLabel.innerHTML = "Soy fotógrafo/a";
+        }
+        if(this.classList.contains("camarografo")){
+            empresaLabel.innerHTML = "Busco camarógrafo/a";
+            profesionaLabel.innerHTML = "Soy camarógrafo/a";
+        }
+        if(this.classList.contains("maquillador")){
+            empresaLabel.innerHTML = "Busco maquillador/a";
+            profesionaLabel.innerHTML = "Soy maquillador/a";
+        }
+        if(this.classList.contains("productor")){
+            empresaLabel.innerHTML = "Busco productor/a";
+            profesionaLabel.innerHTML = "Soy productor/a";
+        }
+        if(this.classList.contains("editor")){
+            empresaLabel.innerHTML = "Busco editor/a";
+            profesionaLabel.innerHTML = "Soy editor/a";
+        }
+        if(this.classList.contains("equipos")){
+            empresaLabel.innerHTML = "Quiero alquilar equipos";
+            profesionaLabel.innerHTML = "Alquilo equipos";
+        }
+        if(this.classList.contains("locaciones")){
+            empresaLabel.innerHTML = "Quiero alquilar locaciones";
+            profesionaLabel.innerHTML = "Alquilo locaciones";
+        }
+})
+}
+
+empresa.addEventListener('click', function(){
+    submit.value = "Encontrar profesional";
+});
+profesional.addEventListener('click', function(){
+    submit.value = "Encontrar trabajo";
+});

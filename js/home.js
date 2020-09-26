f747b603061d94f6ef5a427ca77fd399c6d6fe73
morena <morena@dsklabs.com>
2020-09-26 03:07:08 -0300

    avance 25/9

--- js/home.js
+++ js/home.js
@@ -94,62 +94,128 @@ for (var i = 0; i < franjaOption.length; i++) {
 
 /////// canje ///////
 
+var canje = document.getElementById("canje");
 var submitCanje = document.getElementById("submit-canje");
 var ofrezcoCanje = document.getElementById("ofrezco-canje");
 var buscoCanje = document.getElementById("busco-canje");
 var profesionOfrezco = document.getElementById("profesion-ofrezco");
 var profesionBusco = document.getElementById("profesion-busco");
 
+canje.addEventListener("submit", function ofrezcoCanje(e){
+    e.preventDefault();
+    window.location.href = 'search/ofrezco-canje.html';
+});
+
 ofrezcoCanje.addEventListener('click', function(){
     profesionOfrezco.style.display = "block";
     profesionBusco.style.display = "none";
     submitCanje.value = "Buscar profesional";
+    canje.removeEventListener("submit", buscoCanje);
+    canje.addEventListener("submit", function ofrezcoCanje(e){
+        e.preventDefault();
+        window.location.href = 'search/ofrezco-canje.html';
+    });
 });
 
 buscoCanje.addEventListener('click', function(){
     profesionOfrezco.style.display = "none";
     profesionBusco.style.display = "block";
     submitCanje.value = "Buscar canje";
+    canje.removeEventListener("submit", ofrezcoCanje);
+    canje.addEventListener("submit", function buscoCanje(e){
+        e.preventDefault();
+        window.location.href = 'search/busco-canje.html';
+    });
 });
 
 /////// modelo ///////
 
+var modelo = document.getElementById("modelo");
 var buscoModelo = document.getElementById("busco-modelo");
 var soyModelo = document.getElementById("soy-modelo");
 var submitModelo = document.getElementById("submit-modelo");
 
+modelo.addEventListener("submit", function buscoModelo(e){
+    e.preventDefault();
+    window.location.href = 'search/busco-modelo.html';
+});
+
+
 buscoModelo.addEventListener('click', function(){
     submitModelo.value = "Buscar modelo";
+    modelo.removeEventListener("submit", soyModelo);
+    modelo.addEventListener("submit", function buscoModelo(e){
+        e.preventDefault();
+        window.location.href = 'search/busco-modelo.html';
+    });
 });
 
 soyModelo.addEventListener('click', function(){
     submitModelo.value = "Buscar trabajo";
+    modelo.removeEventListener("submit", buscoModelo);
+    modelo.addEventListener("submit", function soyModelo(e){
+        e.preventDefault();
+        window.location.href = 'search/soy-modelo.html';
+    });
 });
 
 /////// influencer ///////
 
+var influencer = document.getElementById("influencer");
 var buscoInfluencer = document.getElementById("busco-influencer");
 var soyInfluencer = document.getElementById("soy-influencer");
 var submitInfluencer = document.getElementById("submit-influencer");
 
+influencer.addEventListener("submit", function buscoInfluencer(e){
+    e.preventDefault();
+    window.location.href = 'search/busco-influencer.html';
+});
+
 buscoInfluencer.addEventListener('click', function(){
     submitInfluencer.value = "Buscar influencer";
+    influencer.removeEventListener("submit", soyInfluencer);
+    influencer.addEventListener("submit", function buscoInfluencer(e){
+        e.preventDefault();
+        window.location.href = 'search/busco-influencer.html';
+    });
 });
 
 soyInfluencer.addEventListener('click', function(){
     submitInfluencer.value = "Buscar trabajo";
+    influencer.removeEventListener("submit", buscoInfluencer);
+    influencer.addEventListener("submit", function soyInfluencer(e){
+        e.preventDefault();
+        window.location.href = 'search/soy-influencer.html';
+    });
 });
 
 /////// alquiler ///////
 
+var alquiler = document.getElementById("alquiler");
 var buscoAlquilar = document.getElementById("busco-alquilar");
 var alquilo = document.getElementById("alquilo");
 var submitAlquiler = document.getElementById("submit-alquiler");
 
+
+alquiler.addEventListener("submit", function buscoAlquilar(e){
+    e.preventDefault();
+    window.location.href = 'search/busco-alquilar.html';
+});
+
 buscoAlquilar.addEventListener('click', function(){
     submitAlquiler.value = "Buscar alquileres";
+    alquiler.removeEventListener("submit", alquilo);
+    alquiler.addEventListener("submit", function buscoAlquilar(e){
+        e.preventDefault();
+        window.location.href = 'search/busco-alquilar.html';
+    });
 });
 
 alquilo.addEventListener('click', function(){
     submitAlquiler.value = "Publicar";
+    alquiler.removeEventListener("submit", buscoAlquilar);
+    alquiler.addEventListener("submit", function alquilo(e){
+        e.preventDefault();
+        window.location.href = 'search/alquilo.html';
+    });
 });
